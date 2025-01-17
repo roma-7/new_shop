285cf9725d98e0a3ff3a10661d9c5cbc979ea8c2
Ramazan <125178278+roma-7@users.noreply.github.com>
2025-01-17 16:49:22 +0600

    Исправлены настройки медиа и статических файлов для режима отладки

--- shop/urls.py
+++ shop/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path("analytics/", include("analytics.urls", namespace="analytics")),
 ] 
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
